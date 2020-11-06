9514216b0c42922e6411d54a8407c3eb34533e29
Moaberdã Gomes Freire <moaberdan@gmail.com>
2020-11-06 18:32:18 -0300

    google_maps_flutter_platform-interface: add support heatmap

---
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/utils/heatmap.dart
@@ -0,0 +1,16 @@
+﻿import '../types.dart';
+
+Map<HeatmapId, Heatmap> keyByHeatmapId(Iterable<Heatmap> heatmaps) {
+  if (heatmaps == null) {
+    return <HeatmapId, Heatmap>{};
+  }
+  return Map<HeatmapId, Heatmap>.fromEntries(heatmaps.map((Heatmap heatmap) =>
+      MapEntry<HeatmapId, Heatmap>(heatmap.heatmapId, heatmap.clone())));
+}
+
+List<Map<String, dynamic>> serializeHeatmapSet(Set<Heatmap> heatmaps) {
+  if (heatmaps == null) {
+    return null;
+  }
+  return heatmaps.map<Map<String, dynamic>>((Heatmap p) => p.toJson()).toList();
+}
